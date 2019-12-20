b208a0a2c5c2740cacf8d9e22ca653fe553bf9a4
Douglas Drinka <ddrinka@gmail.com>
2019-12-20 11:31:58 -0600

    Fixed quotes in createRelease upload_url. Removed optional arguments.

--- tools/createRelease.sh
+++ tools/createRelease.sh
@@ -2,14 +2,12 @@
 
 token=$1
 repo=$2
-git_tag=$3
-release_version=$4
-text=$5
-path=$6
-filename=$7
+release_version=$3
+path=$4
+filename=$5
 
 upload_url=$(curl -s -H "Authorization: token $token"  \
-     -d '{"tag_name": "$git_tag", "name": "$release_version", "body": "$text"}'  \
+     -d "{\"tag_name\": \"$release_version\", \"prerelease\": \"true\"}"  \
      "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url')
 
 upload_url="${upload_url%\{*}"
@@ -19,4 +17,4 @@ echo "uploading asset to release to url : $upload_url"
 curl -s -H "Authorization: token $token"  \
         -H "Content-Type: application/zip" \
         --data-binary "$path/$filename"  \
-        "$upload_url?name=$filename&label=$filename"
+        "$upload_url?name=$filename"
