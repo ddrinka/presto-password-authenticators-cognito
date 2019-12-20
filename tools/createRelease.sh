7086e6aefd3f35d72835e36e2a875df71b676de6
Douglas Drinka <ddrinka@gmail.com>
2019-12-20 12:59:08 -0600

    Removed debugging output.

--- tools/createRelease.sh
+++ tools/createRelease.sh
@@ -13,8 +13,6 @@ upload_url=$(curl -s -H "Authorization: token $token"  \
 
 upload_url="${upload_url%\{*}"
 
-echo "uploading asset to release to url : $upload_url"
-
 curl -s -H "Authorization: token $token"  \
         -H "Content-Type: application/zip" \
         --data-binary "@$path/$filename"  \
